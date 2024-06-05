04c169dcb89f617427db365e19105f0b8c53360f
Сергей <sergey.vasilev.98@mail.ru>
2024-06-05 22:18:50 +0300

    add something

--- Work/func.py
+++ Work/func.py
@@ -1,10 +1,15 @@
 def get_operations():
-    with open("operations.json", "r", encoding="utf-8") as file:
-        print(file.read())
+    with open("operations.json", "w", encoding="utf-8") as file:
+        info = []
+        for item,key in file:
+            if item == "state" and key == "EXECUTED":
+                info.append(file)
+        return f"{info}"
+
 
 def format_data(srt_data: str):
     my_data = srt_data.split("T")[0].split("-")
     return ".".join(my_data[::-1])
 
 
-get_operations()
+print(file = open("operations.json", "w", encoding="utf-8"))
