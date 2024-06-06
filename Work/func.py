e1299eaace9baaf627f31a9371ad86b7bcd4dac8
Сергей <sergey.vasilev.98@mail.ru>
2024-06-06 22:07:16 +0300

    add func stealth card number

--- Work/func.py
+++ Work/func.py
@@ -20,7 +20,12 @@ def format_data(srt_data: str):
 
 def stealth_account_number(account_number):
     info = account_number.split()
-    return f"{info[0]} ****************{info[1][-4:]}"
+    return f"{info[0]} **{info[1][-4:]}"
+
+# "Visa Classic 6831982476737658"
+def stealth_card_number(card_number):
+    info = card_number.rsplit(" ", 1)
+    return f"{info[0]} {info[1][0:4]} {info[1][4:6]}** **** {info[1][-4:]}"
 
 
-print(stealth_account_number("Счет 64686473678894779589"))
+print(stealth_card_number("Visa Classic 6831982476737658"))
