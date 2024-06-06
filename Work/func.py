97b419d3fee69b4eea7514b57a9adc123188e1a0
Сергей <sergey.vasilev.98@mail.ru>
2024-06-06 20:41:06 +0300

    get last 5 executed operations

--- Work/func.py
+++ Work/func.py
@@ -1,9 +1,16 @@
 import json
-
+from datetime import datetime
 
 def get_operations():
     with open("operations.json", encoding="utf-8") as file:
         return json.load(file)
+
+
+def get_last_5_executed(operations):
+    executed_operations = list(filter(lambda x: x.get('state') == 'EXECUTED', operations))
+    executed_operations.sort(key=lambda x: datetime.fromisoformat(x['date']), reverse=True)
+    last_5_executed = executed_operations[:5]
+    return last_5_executed
 
 
 def format_data(srt_data: str):
@@ -11,4 +18,4 @@ def format_data(srt_data: str):
     return ".".join(my_data[::-1])
 
 
-print(get_operations())
+print(get_last_5_executed(get_operations()))
